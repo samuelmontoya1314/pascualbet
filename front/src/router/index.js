011fb03e2292dc24f8f8a1403aa8d2b8867b1f55
juan1417 <juan.machado210@pascualbravo.edu.co>
2025-10-14 06:12:18 -0500

    cambia el historial del enrutador a createWebHashHistory en lugar de createWebHistory

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import index from '../components/index.vue';
 import login from '../components/login.vue';
 import SlotM from '../components/games/slotM.vue';
@@ -48,7 +48,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
